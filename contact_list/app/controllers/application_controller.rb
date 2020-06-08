5e6c20edc3bb8e6a5d9d2d14eba60a635cb35754
fernandoAOliveira <fernandous4@yahoo.com.br>
2020-06-08 19:07:29 -0300

    Relacionamento entre contato e usuario

--- contact_list/app/controllers/application_controller.rb
+++ contact_list/app/controllers/application_controller.rb
@@ -3,5 +3,14 @@ class ApplicationController < ActionController::Base
 
     include SessionHelper
 
+    private
+    def require_logged_in_user
+       unless user_signed_in? 
+        flash[:danger] = 'Àrea restrita. Por favor, realize o login'
+        redirect_to entrar_url
+
+    end
+
+end
 end
 
--- contact_list/config/routes.rb
+++ contact_list/config/routes.rb
@@ -7,6 +7,7 @@ get 'sobre', to: 'static_pages#sobre'
 get 'contato', to: 'static_pages#contato'
 get 'entrar', to: 'sessions#new'
 post 'entrar', to: 'sessions#create'
+delete 'sair', to: 'sessions#destroy'
 
 resources :contacts
 resources :users, only: [:new, :create, :show]
